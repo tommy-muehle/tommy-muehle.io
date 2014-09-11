6dc9c8bbee12a79bc6c699460385c1e08a640328
Tommy Muehle <tommy.muehle@gmail.com>
2014-09-11 15:37:43 +0200

    Add yaml service provider

---
+++ src/TM/Provider/YamlConfigServiceProvider.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace TM\Provider;
+
+use Silex\Application;
+use Silex\ServiceProviderInterface;
+use Symfony\Component\Yaml\Yaml;
+
+/**
+ * Class YamlConfigServiceProvider
+ *
+ * @package TM\Provider
+ */
+class YamlConfigServiceProvider implements ServiceProviderInterface
+{
+    /**
+     * @var string
+     */
+    protected $file;
+
+    /**
+     * @param string $file
+     */
+    public function __construct($file)
+    {
+        $this->file = $file;
+    }
+
+    /**
+     * @param \Silex\Application $app
+     */
+    public function register(Application $app)
+    {
+        $config = Yaml::parse($this->file);
+
+        if (isset($app['config']) && is_array($app['config'])) {
+            $app['config'] = array_merge($app['config'], $config);
+        } else {
+            $app['config'] = $config;
+        }
+    }
+
+    /**
+     * @param \Silex\Application $app
+     */
+    public function boot(Application $app)
+    {
+    }
+} 
